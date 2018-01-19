d281627381afa2ff7ea82b6343984142d643c4e8
Student P_10 <studentp_10@felixs-macbook-pro.local>
2018-01-19 11:56:20 +0530

    Initial Commit

---
+++ TextToSpeech/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TextToSpeech
+//
+//  Created by Student P_10 on 19/01/18.
+//  Copyright © 2018 Felix. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TextToSpeech/ViewController.h
@@ -0,0 +1,17 @@
+//
+//  ViewController.h
+//  TextToSpeech
+//
+//  Created by Student P_10 on 19/01/18.
+//  Copyright © 2018 Felix. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <AVFoundation/AVFoundation.h>
+
+@interface ViewController : UIViewController
+
+- (IBAction)buttonPressed:(UIButton *)sender;
+
+@end
+
